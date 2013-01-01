3d9a25b7051974cebbbaface87bab2c40058e7fc
Georgi Todorov <terahz@geodar.com>
2013-01-01 12:42:13 -0500

    Initial Import

---
+++ src/MCP23008.cpp
@@ -0,0 +1,34 @@
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Name        : MCP23008.cpp
+ * Author      : Georgi Todorov
+ * Version     :
+ * Created on  : Jan 1, 2013
+ *
+ * Copyright © 2012 Georgi Todorov  <terahz@geodar.com>
+ */
+
+#include "MCP23008.h"
+
+MCP23008::MCP23008() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MCP23008::~MCP23008() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/MCP23008.h
@@ -0,0 +1,33 @@
+/*
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Name        : MCP23008.h
+ * Author      : Georgi Todorov
+ * Version     :
+ * Created on  : Jan 1, 2013
+ *
+ * Copyright © 2012 Georgi Todorov  <terahz@geodar.com>
+ */
+
+#ifndef MCP23008_H_
+#define MCP23008_H_
+
+class MCP23008 {
+public:
+	MCP23008();
+	virtual ~MCP23008();
+};
+
+#endif /* MCP23008_H_ */
